f7ab4c37159a73d45e421e6c94a0db4f76084f60
MrNaif2018 <chuff184@gmail.com>
2019-03-06 20:15:51 +0300

    Added dev yml file

--- setup.sh
+++ setup.sh
@@ -5,9 +5,8 @@ cat >> .env << EOF
 DB_HOST=database
 MEMCACHED_URL=http://memcached:11211
 CELERY_BROKER_URL=amqp://rabbitmq
-CHANNEL_LAYERS_HOST=amqp://guest:guest@rabbitmq/asgi
+CHANNEL_LAYERS_HOST=amqp://rabbitmq
 RPC_URL=http://daemon:5000
-DEBUG=True
 EOF
 rm -rf gui/migrations
 cd ..
