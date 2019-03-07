b1dcb2a865d8c8c7707ca0a10cfc56edd473c40b
MrNaif2018 <39452697+MrNaif2018@users.noreply.github.com>
2019-03-07 22:42:01 +0300

    No input in collectstatic and others

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -ex
-python3 manage.py makemigrations
-python3 manage.py makemigrations gui
-python3 manage.py migrate
-python3 manage.py collectstatic
+python3 manage.py makemigrations --no-input
+python3 manage.py makemigrations gui --no-input
+python3 manage.py migrate --no-input
+python3 manage.py collectstatic --no-input
 python3 manage.py runserver 0.0.0.0:8000
