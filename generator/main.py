466ed4c42ac4dc524495236092889a09e847a9fc
MrNaif2018 <chuff184@gmail.com>
2019-07-29 12:57:48 +0300

    Initial components adding

--- generator/main.py
+++ generator/main.py
@@ -1,19 +1,71 @@
 import shutil
 from os import getenv
-from os.path import join as path_join
+from os.path import join as path_join, exists
+from typing import List, Set, Set
 
 import oyaml as yaml
 
-COMPOSE_DIR = 'compose'
+COMPOSE_DIR = 'compose' if getenv("IN_DOCKER") else '../compose'
 BACKEND = path_join(COMPOSE_DIR, 'backend-compose.yml')
 FRONTEND = path_join(COMPOSE_DIR, 'frontend-compose.yml')
 BACKEND_DEV = path_join(COMPOSE_DIR, 'backend-compose.dev.yml')
 FRONTEND_DEV = path_join(COMPOSE_DIR, 'frontend-compose.dev.yml')
+COMPONENTS_DIR = 'docker-components'
 GENERATED_NAME = 'generated.yml'
 
-to_install = getenv("BITCART_INSTALL", "all")
-# double check
-if not to_install:
+CRYPTOS = {
+    'btc': {'component': 'bitcoin'},
+    'ln': {'component': 'lightning'}
+}
+BACKEND_COMPONENTS = ['backend', 'dramatiq', 'postgres', 'redis']
+FRONTEND_COMPONENTS = ['frontend']
+
+
+def add_components() -> Set[str]:
+    components: Set[str] = set()
+    # components.update()
+    # add daemons
+    for i in range(1, 10):
+        crypto = getenv(f'BITCART_CRYPTO{i}')
+        if crypto:
+            value = CRYPTOS.get(crypto)
+            if value:
+                components.add(value["component"])
+    # bitcart backend and frontend
+    to_install = getenv("BITCART_INSTALL", "all")
+    if not to_install:
+        to_install = "all"
+    if to_install == "all":
+        components.update(BACKEND_COMPONENTS + FRONTEND_COMPONENTS)
+    elif to_install == "backend":
+        components.update(BACKEND_COMPONENTS)
+    elif to_install == "frontend":
+        components.update(FRONTEND_COMPONENTS)
+    return components
+
+
+def load_component(component: str):
+    path = path_join(COMPONENTS_DIR, component + ".yml")
+    if not exists(path):
+        return
+    with open(path) as f:
+        data = yaml.load(f, Loader=yaml.SafeLoader)
+        print(data)
+
+
+def generate(components: Set[str]):
+    # generated yaml
+    data = {}
+    for i in components:
+        load_component(i)
+    with open(path_join(COMPOSE_DIR, GENERATED_NAME), "w") as f:
+        yaml.dump(data, f, default_flow_style=False)
+
+
+components = add_components()
+print(components)
+generate(components)
+'''if not to_install:
     to_install = "all"
 if to_install == "all":
     with open(BACKEND) as f:
@@ -35,4 +87,4 @@ else:
     else:
         f_name = FRONTEND
 
-    shutil.copy2(f_name, path_join(COMPOSE_DIR, GENERATED_NAME))
+    shutil.copy2(f_name, path_join(COMPOSE_DIR, GENERATED_NAME))'''
