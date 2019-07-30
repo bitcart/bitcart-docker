378c665dffe43c6f8198cc39f21f64f01f46b644
MrNaif2018 <chuff184@gmail.com>
2019-07-30 13:53:53 +0300

    Small fixes

--- generator/rules/bitcart_noreverseproxy.py
+++ generator/rules/bitcart_noreverseproxy.py
@@ -2,8 +2,9 @@ from constants import CRYPTO_COMPONENTS
 
 
 def rule(services):
-    items = ["backend", "frontend"]
-    items.extend(CRYPTO_COMPONENTS)
-    for i in items:
-        if services.get(i):
-            services[i]["ports"] = services[i].get("expose", [])
+    if not services.get("nginx"):
+        items = ["backend", "frontend"]
+        items.extend(CRYPTO_COMPONENTS)
+        for i in items:
+            if services.get(i):
+                services[i]["ports"] = services[i].get("expose", [])
