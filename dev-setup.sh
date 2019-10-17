d11decbeb132d423febb0e6df704fdcf568f9898
MrNaif2018 <chuff184@gmail.com>
2019-10-17 20:39:20 +0300

    Fixes for daemon connection

--- dev-setup.sh
+++ dev-setup.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 sudo apt install -y git
 branch=${1:-master}
-rm -rf bitcart && git clone https://github.com/MrNaif2018/bitcart -b $branch compose/bitcart
+rm -rf compose/bitcart && git clone https://github.com/MrNaif2018/bitcart -b $branch compose/bitcart
 cd compose/bitcart
 cat > conf/.env << EOF
 DB_HOST=database
 CHANNEL_LAYERS_HOST=redis://redis
 CACHE_REDIS_URL=redis://redis
 DRAMATIQ_REDIS_URL=redis://redis
-RPC_URL=http://daemon:5000
+RPC_URL=http://bitcoin:5000
 EOF
 cd ../..
