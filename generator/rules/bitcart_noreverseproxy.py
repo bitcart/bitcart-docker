1c1917851d1943e911b5dc44079b75831b465291
MrNaif2018 <chuff184@gmail.com>
2019-10-21 17:43:14 +0300

    Added admin panel to docker, removed outdated components

--- generator/rules/bitcart_noreverseproxy.py
+++ generator/rules/bitcart_noreverseproxy.py
@@ -3,7 +3,7 @@ from constants import CRYPTO_COMPONENTS
 
 def rule(services):
     if not services.get("nginx"):
-        items = ["backend", "frontend"]
+        items = ["backend", "store", "admin"]
         items.extend(CRYPTO_COMPONENTS)
         for i in items:
             if services.get(i):
--- generator/rules/frontend.py
+++ generator/rules/frontend.py
@@ -1,5 +1,8 @@
 def rule(services):
-    front = services.get("frontend")
+    store = services.get("store")
+    admin = services.get("admin")
     back = services.get("backend")
-    if back and front:
-        front["links"] = ["backend"]
+    if back and store:
+        store["links"] = ["backend"]
+    if back and admin:
+        admin["links"] = ["backend"]
