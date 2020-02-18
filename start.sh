a9b2591c1ca9c73c8bcd928ad6d36d53bf01fb7c
MrNaif2018 <chuff184@gmail.com>
2020-02-18 14:29:32 +0300

    Added named pipe for commands processing

--- start.sh
+++ start.sh
@@ -12,4 +12,8 @@ fi
 . ${BASH_PROFILE_SCRIPT}
 
 cd "$BITCART_BASE_DIRECTORY"
+# setup pipe and it's listener
+mkfifo queue
+nohup sh -c "tail -f queue | sh" > /dev/null &
+echo $! > listener.pid
 USER_UID=${UID} USER_GID=${GID} docker-compose -f compose/generated.yml up --remove-orphans -d
--- stop.sh
+++ stop.sh
@@ -12,4 +12,6 @@ fi
 . ${BASH_PROFILE_SCRIPT}
 
 cd "$BITCART_BASE_DIRECTORY"
+# stop listener
+kill $(cat listener.pid)
 USER_UID=${UID} USER_GID=${GID} docker-compose -f compose/generated.yml down
