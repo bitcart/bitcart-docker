294d6c676186155a8e0d0343ceb4b5d6a44d4ea5
MrNaif2018 <chuff184@gmail.com>
2020-06-05 15:08:48 +0300

    Silence nohup output

--- start.sh
+++ start.sh
@@ -8,6 +8,6 @@ get_profile_file "$NAME" false
 cd "$BITCART_BASE_DIRECTORY"
 # setup pipe and it's listener
 mkfifo queue &> /dev/null
-nohup sh -c "tail -f queue | sh" > /dev/null &
+nohup sh -c "tail -f queue | sh" &> /dev/null &
 echo $! > listener.pid
 USER_UID=${UID} USER_GID=${GID} docker-compose -p "$NAME" -f compose/generated.yml up --remove-orphans -d
