57dff249dc1a8031e7831eb2388e01eda8f9c635
MrNaif2018 <chuff184@gmail.com>
2020-06-05 14:29:15 +0300

    Use env for getting env vars

--- build.sh
+++ build.sh
@@ -11,5 +11,5 @@ docker run -v "$PWD/compose:/app/compose" \
     -e "BITCART_CRYPTOS=${BITCART_CRYPTOS:-btc}" \
     -e "BITCART_REVERSEPROXY=${BITCART_REVERSEPROXY:-nginx-https}" \
     -e "BITCART_ADDITIONAL_COMPONENTS=$BITCART_ADDITIONAL_COMPONENTS" \
-    --env-file <(set | awk -F "=" '{print "\n"$0}' | grep "BITCART_") \
+    --env-file <(env | grep BITCART_) \
     --rm $BITCARTGEN_DOCKER_IMAGE
