694cef011c7496077557f90324a3c0ea840df2a6
MrNaif2018 <chuff184@gmail.com>
2020-06-28 13:28:06 +0300

    Don't store .git folder when checking out repo

--- dev-setup.sh
+++ dev-setup.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 sudo apt install -y git
 branch=${1:-master}
-rm -rf compose/bitcart && git clone https://github.com/MrNaif2018/bitcart -b $branch compose/bitcart
+rm -rf compose/bitcart && git clone --depth=1 https://github.com/MrNaif2018/bitcart -b $branch compose/bitcart
 cd compose/bitcart
+rm -rf .git
 cat > conf/.env << EOF
 DB_HOST=database
 REDIS_HOST=redis://redis
