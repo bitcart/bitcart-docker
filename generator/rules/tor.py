181006f98feda588d58ccd8515074540db3c85e8
MrNaif2018 <chuff184@gmail.com>
2020-07-30 13:33:26 +0300

    Added proxy support when tor is enabled

--- generator/rules/tor.py
+++ generator/rules/tor.py
@@ -1,4 +1,4 @@
-from constants import CRYPTO_COMPONENTS
+from constants import CRYPTOS
 from utils import modify_key
 
 
@@ -15,3 +15,8 @@ def rule(services):
                         environment[
                             "HIDDENSERVICE_IP"
                         ] = "172.17.0.1"  # TODO: check if always available
+        for env_name, service in CRYPTOS.items():
+            service_name = service["component"]
+            if services.get(service_name):
+                with modify_key(services[service_name], "environment") as environment:
+                    environment[f"{env_name.upper()}_PROXY_URL"] = "socks5://tor:9050"
