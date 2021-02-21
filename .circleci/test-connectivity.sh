0a3c3b70f911e6e68844286b3b30170cb531344c
MrNaif2018 <chuff184@gmail.com>
2021-02-21 20:12:24 +0300

    Fix

--- .circleci/test-connectivity.sh
+++ .circleci/test-connectivity.sh
@@ -2,7 +2,7 @@
 
 echo "Trying to connect to bitcartcc..."
 while true; do
-    if [ "$(curl -sL -w "%{http_code}\\n" "http://localhost/" -o /dev/null)" == "200" ]; then
+    if [ "$(curl -sL -w "%{http_code}\\n" "http://localhost/api" -o /dev/null)" == "200" ]; then
         echo "Successfully contacted BitcartCC"
         break
     fi
