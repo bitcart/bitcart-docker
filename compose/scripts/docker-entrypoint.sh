4ff0fc9b2e2b56ef039f19c4f1a651c36668ca3f
MrNaif2018 <chuff184@gmail.com>
2021-04-05 23:03:01 +0300

    Fix ssh key generation

--- compose/scripts/docker-entrypoint.sh
+++ compose/scripts/docker-entrypoint.sh
@@ -6,20 +6,20 @@ set -ex
 # Make host.docker.internal work (on linux-based docker engines)
 echo "$(/sbin/ip route|awk '/default/ { print $3 }')  host.docker.internal" >> /etc/hosts
 
-if [ ! -z "$BITCART_SSH_KEY_FILE" ] && ! [ -f "$BITCART_SSH_KEY_FILE" ]; then
+if [ ! -z "$SSH_KEY_FILE" ] && ! [ -f "$SSH_KEY_FILE" ]; then
     echo "Creating BitcartCC SSH key File..."
-    ssh-keygen -t rsa -f "$BITCART_SSH_KEY_FILE" -q -P "" -m PEM -C bitcartcc > /dev/null
-    if [ -f "$BITCART_SSH_AUTHORIZED_KEYS" ]; then
+    ssh-keygen -t rsa -f "$SSH_KEY_FILE" -q -P "" -m PEM -C bitcartcc > /dev/null
+    if [ -f "$SSH_AUTHORIZED_KEYS" ]; then
         # Because the file is mounted, sed -i does not work
-        sed '/bitcartcc$/d' "$BITCART_SSH_AUTHORIZED_KEYS" > "$BITCART_SSH_AUTHORIZED_KEYS.new"
-        cat "$BITCART_SSH_AUTHORIZED_KEYS.new" > "$BITCART_SSH_AUTHORIZED_KEYS"
-        rm -rf "$BITCART_SSH_AUTHORIZED_KEYS.new"
+        sed '/bitcartcc$/d' "$SSH_AUTHORIZED_KEYS" > "$SSH_AUTHORIZED_KEYS.new"
+        cat "$SSH_AUTHORIZED_KEYS.new" > "$SSH_AUTHORIZED_KEYS"
+        rm -rf "$SSH_AUTHORIZED_KEYS.new"
     fi
 fi
 
-if [ ! -z "$BITCART_SSH_KEY_FILE" ] && [ -f "$BITCART_SSH_AUTHORIZED_KEYS" ] && ! grep -q "bitcartcc$" "$BITCART_SSH_AUTHORIZED_KEYS"; then
+if [ ! -z "$SSH_KEY_FILE" ] && [ -f "$SSH_AUTHORIZED_KEYS" ] && ! grep -q "bitcartcc$" "$SSH_AUTHORIZED_KEYS"; then
     echo "Adding BitcartCC SSH key to authorized keys"
-    cat "$BITCART_SSH_KEY_FILE.pub" >> "$BITCART_SSH_AUTHORIZED_KEYS"
+    cat "$SSH_KEY_FILE.pub" >> "$SSH_AUTHORIZED_KEYS"
 fi
 
 alembic upgrade head
