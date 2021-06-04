202ecbf45d5bce34e64562b29657b135054ff02f
MrNaif2018 <chuff184@gmail.com>
2021-06-04 15:34:40 +0300

    Added migration helpers

---
+++ contrib/upgrades/README.md
@@ -0,0 +1,13 @@
+# Upgrade helpers
+
+This directory contains a list of files to help upgrade to various BitcartCC versions.
+
+To apply any fix (note, scripts are one-time only in most cases), just run:
+
+`contrib/upgrades/upgrade-to-version.sh`
+
+Your docker deployment should be running.
+
+Current list:
+
+- `upgrade-to-5000.sh`, helps to upgrade to BitcartCC 0.5.0.0, run this in case you get a migration error (invalid foreign key constraints names). It might be required for older BitcartCC deployments, requires a running database container
---
+++ contrib/upgrades/upgrade-to-0500.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+docker exec -it compose_database_1 psql -U postgres bitcart << EOF
+ALTER TABLE discountsxproducts RENAME CONSTRAINT discountsxproducts_discount_id_fkey TO discountsxproducts_discount_id_discounts_fkey;
+ALTER TABLE discountsxproducts RENAME CONSTRAINT discountsxproducts_product_id_fkey TO discountsxproducts_product_id_products_fkey;
+ALTER TABLE discounts RENAME CONSTRAINT discounts_user_id_fkey TO discounts_user_id_users_fkey;
+ALTER TABLE paymentmethods RENAME CONSTRAINT paymentmethods_invoice_id_fkey TO paymentmethods_invoice_id_invoices_fkey;
+ALTER TABLE productsxinvoices RENAME CONSTRAINT productsxinvoices_invoice_id_fkey TO productsxinvoices_invoice_id_invoices_fkey;
+ALTER TABLE invoices RENAME CONSTRAINT invoices_store_id_fkey TO invoices_store_id_stores_fkey;
+ALTER TABLE invoices RENAME CONSTRAINT invoices_user_id_fkey TO invoices_user_id_users_fkey;
+ALTER TABLE notificationsxstores RENAME CONSTRAINT notificationsxstores_notification_id_fkey TO notificationsxstores_notification_id_notifications_fkey;
+ALTER TABLE notifications RENAME CONSTRAINT notifications_user_id_fkey TO notifications_user_id_users_fkey;
+ALTER TABLE notificationsxstores RENAME CONSTRAINT notificationsxstores_store_id_fkey TO notificationsxstores_store_id_stores_fkey;
+ALTER TABLE productsxinvoices RENAME CONSTRAINT productsxinvoices_product_id_fkey TO productsxinvoices_product_id_products_fkey;
+ALTER TABLE products RENAME CONSTRAINT products_store_id_fkey TO products_store_id_stores_fkey;
+ALTER TABLE products RENAME CONSTRAINT products_user_id_fkey TO products_user_id_users_fkey;
+ALTER TABLE walletsxstores RENAME CONSTRAINT walletsxstores_store_id_fkey TO walletsxstores_store_id_stores_fkey;
+ALTER TABLE stores RENAME CONSTRAINT stores_user_id_fkey TO stores_user_id_users_fkey;
+ALTER TABLE templates RENAME CONSTRAINT templates_user_id_fkey TO templates_user_id_users_fkey;
+ALTER TABLE wallets RENAME CONSTRAINT wallets_user_id_fkey TO wallets_user_id_users_fkey;
+ALTER TABLE tokens RENAME CONSTRAINT tokens_user_id_fkey TO tokens_user_id_users_fkey;
+ALTER TABLE walletsxstores RENAME CONSTRAINT walletsxstores_wallet_id_fkey TO walletsxstores_wallet_id_wallets_fkey;
+EOF
