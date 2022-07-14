2efcf2ba0be975b6e6326761601d77367bec6451
MrNaif2018 <chuff184@gmail.com>
2022-07-14 21:48:59 +0300

    Add custom web services support

--- generator/rules/04_no_reverseproxy.py
+++ generator/rules/04_no_reverseproxy.py
@@ -4,7 +4,10 @@ from ..utils import custom_port_allowed, env, modify_key
 
 def rule(services, settings):
     no_nginx = not services.get("nginx")
-    items = HOST_COMPONENTS + CRYPTO_COMPONENTS
+    custom_web_services = [
+        key for key, service in services.items() if service.get("environment", {}).get("BITCART_WEBSERVICE", False)
+    ]
+    items = HOST_COMPONENTS + CRYPTO_COMPONENTS + custom_web_services
     for i in items:
         if services.get(i) and custom_port_allowed(i, no_nginx):
             with modify_key(services, i, "expose", [], "ports") as expose:
