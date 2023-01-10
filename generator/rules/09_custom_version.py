cf1aa2de825aeff2a1bab7419be5554a8a6c8c9a
MrNaif2018 <chuff184@gmail.com>
2023-01-10 18:17:59 +0300

    Fix reliability of custom version rule

--- generator/rules/09_custom_version.py
+++ generator/rules/09_custom_version.py
@@ -4,6 +4,7 @@ from generator.utils import env
 def rule(services, settings):
     version = env("VERSION", "stable")
     for i in services:
-        parts = services[i]["image"].split(":")
-        if len(parts) == 2 and parts[1] == "stable" and parts[0].startswith("bitcartcc/"):
-            services[i]["image"] = parts[0] + f":{version}"
+        if "image" in services[i]:
+            parts = services[i]["image"].split(":")
+            if len(parts) == 2 and parts[1] == "stable" and parts[0].startswith("bitcartcc/"):
+                services[i]["image"] = parts[0] + f":{version}"
