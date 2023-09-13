e70398cc83a3aa1ed94ffde5373d8a957e26895b
MrNaif2018 <chuff184@gmail.com>
2023-09-14 01:42:35 +0300

    Fixes

--- contrib/upgrades/upgrade-to-0800.sh
+++ contrib/upgrades/upgrade-to-0800.sh
@@ -3,9 +3,11 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 . "${SCRIPT_DIR}/../../helpers.sh"
 load_env
 
-try rm /etc/profile.d/bitcartcc-env$SCRIPTS_POSTFIX.sh
-try rm $HOME/bitcartcc-env$SCRIPTS_POSTFIX.sh
+try mv /etc/profile.d/bitcartcc-env$SCRIPTS_POSTFIX.sh /etc/profile.d/bitcart-env$SCRIPTS_POSTFIX.sh
+try mv $HOME/bitcartcc-env$SCRIPTS_POSTFIX.sh $HOME/bitcart-env$SCRIPTS_POSTFIX.sh
 try systemctl disable bitcartcc$SCRIPTS_POSTFIX.service
 try systemctl stop bitcartcc$SCRIPTS_POSTFIX.service
-try rm /etc/systemd/system/bitcartcc$SCRIPTS_POSTFIX.service
+try mv /etc/systemd/system/bitcartcc$SCRIPTS_POSTFIX.service /etc/systemd/system/bitcart$SCRIPTS_POSTFIX.service
 try systemctl daemon-reload
+try systemctl enable bitcart$SCRIPTS_POSTFIX.service
+try systemctl start bitcart$SCRIPTS_POSTFIX.service
