9c848be75d5f5c428e1e4c71edee7a3334236757
MrNaif2018 <chuff184@gmail.com>
2025-07-08 12:14:51 +0300

    Fix rootpath env for worker in one-domain mode

--- generator/rules/03_root_path.py
+++ generator/rules/03_root_path.py
@@ -3,13 +3,27 @@ from os.path import join as urljoin
 from generator.utils import modify_key
 
 
+def _modify_backend_env(services, service_name, settings, STORE_AVAILABLE, ADMIN_AVAILABLE):
+    if not services.get(service_name):
+        return
+    if service_name == "backend" and (ADMIN_AVAILABLE or STORE_AVAILABLE):
+        with modify_key(services, service_name, "environment") as environment:
+            environment["BITCART_BACKEND_ROOTPATH"] = environment["BITCART_BACKEND_ROOTPATH"].replace("-}", "-/api}")
+    if ADMIN_AVAILABLE:
+        with modify_key(services, service_name, "environment") as environment:
+            if STORE_AVAILABLE:
+                environment["BITCART_ADMIN_HOST"] = urljoin(settings.HOST or "", "admin")
+                environment["BITCART_ADMIN_ROOTPATH"] = environment["BITCART_ADMIN_ROOTPATH"].replace("/", "/admin")
+            else:
+                environment["BITCART_ADMIN_HOST"] = settings.HOST or ""
+
+
 def rule(services, settings):
     if not settings.ONE_DOMAIN_MODE:
         return
     API_URL = urljoin(settings.API_URL, "api")
     STORE_AVAILABLE = services.get("store")
     ADMIN_AVAILABLE = services.get("admin")
-    BACKEND_AVAILABLE = services.get("backend")
     # replace defaults
     if STORE_AVAILABLE:
         with modify_key(services, "store", "environment") as environment:
@@ -25,13 +39,5 @@ def rule(services, settings):
     elif ADMIN_AVAILABLE:
         with modify_key(services, "admin", "environment") as environment:
             environment["BITCART_ADMIN_API_URL"] = API_URL
-    if BACKEND_AVAILABLE and (ADMIN_AVAILABLE or STORE_AVAILABLE):
-        with modify_key(services, "backend", "environment") as environment:
-            environment["BITCART_BACKEND_ROOTPATH"] = environment["BITCART_BACKEND_ROOTPATH"].replace("-}", "-/api}")
-    if BACKEND_AVAILABLE and ADMIN_AVAILABLE:
-        with modify_key(services, "backend", "environment") as environment:
-            if STORE_AVAILABLE:
-                environment["BITCART_ADMIN_HOST"] = urljoin(settings.HOST or "", "admin")
-                environment["BITCART_ADMIN_ROOTPATH"] = environment["BITCART_ADMIN_ROOTPATH"].replace("/", "/admin")
-            else:
-                environment["BITCART_ADMIN_HOST"] = settings.HOST or ""
+    _modify_backend_env(services, "backend", settings, STORE_AVAILABLE, ADMIN_AVAILABLE)
+    _modify_backend_env(services, "worker", settings, STORE_AVAILABLE, ADMIN_AVAILABLE)
